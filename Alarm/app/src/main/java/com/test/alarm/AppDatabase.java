08c12fd52c01918f40f6fd91aa112492457731a4
주효진 <hyojin_030@naver.com>
2023-03-17 16:51:37 +0900

    Room Test

---
+++ Alarm/app/src/main/java/com/test/alarm/AppDatabase.java
@@ -0,0 +1,30 @@
+package com.test.alarm;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.DatabaseConfiguration;
+import androidx.room.InvalidationTracker;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteOpenHelper;
+
+@Database(entities = {User.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract UserDao userDao();
+
+    @Override
+    public void clearAllTables() {
+
+    }
+
+    @NonNull
+    @Override
+    protected InvalidationTracker createInvalidationTracker() {
+        return null;
+    }
+
+    @NonNull
+    @Override
+    protected SupportSQLiteOpenHelper createOpenHelper(@NonNull DatabaseConfiguration databaseConfiguration) {
+        return null;
+    }
+}
--- Alarm/app/src/main/java/com/test/alarm/MainActivity.java
+++ Alarm/app/src/main/java/com/test/alarm/MainActivity.java
@@ -1,8 +1,12 @@
 package com.test.alarm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +15,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                AppDatabase db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "database-name").build();
+
+                UserDao userDao = db.userDao();
+
+                User user = new User();
+                user.setUid(0000);
+                user.setFirstName("first");
+                user.setLastName("last");
+
+                TestData testData = new TestData();
+                testData.setData1(1111);
+                testData.setData2(2222);
+
+                user.setTestData(testData);
+
+                userDao.insertAll(user);
+
+                List<User> users = userDao.getAll();
+
+                for (User test : users) {
+                    Log.d("TEST", "id : " + test.getUid());
+                    Log.d("TEST", "first : " + test.getFirstName());
+                    Log.d("TEST", "last : " + test.getLastName());
+                    Log.d("TEST", "testdata 1 : " + test.getTestData().getData1());
+                    Log.d("TEST", "testdata 2 : " + test.getTestData().getData2());
+                }
+            }
+        }).start();
 
 
 
