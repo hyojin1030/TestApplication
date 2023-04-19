d9e6ec80f87e999b530f38d55af3a173563a9fdf
주효진 <hyojin_030@naver.com>
2023-04-19 14:03:18 +0900

    test commit

--- testapplink/app/src/main/java/com/mdpert/testapplink/MainActivity.java
+++ testapplink/app/src/main/java/com/mdpert/testapplink/MainActivity.java
@@ -23,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.d("TAG", "branch test");
+
         DynamicLink dynamicLink = FirebaseDynamicLinks.getInstance().createDynamicLink()
                 .setLink(Uri.parse("https://www.example.com/"))
                 .setDomainUriPrefix("https://example.page.link")
